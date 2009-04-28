61198281a25c8106aa20b152d574f8889c0ba7ed
Kyo Nagashima <kyo@hail2u.net>
2009-04-29 08:08:59 +0900

    change compression utility from 7za to zip

--- build.sh
+++ build.sh
@@ -14,8 +14,8 @@ rm -rf $TEMP_DIR
 mkdir $TEMP_DIR
 cp -r $DIRS $FILES $TEMP_DIR
 cd $TEMP_DIR
-7za a -tzip "$NAME.xpi" -mx=9 * -r > /dev/null 2>&1
-# zip -rq9X "$NAME.xpi" *
+# 7za a -tzip "$NAME.xpi" -mx=9 * -r > /dev/null 2>&1
+zip -rq9X "$NAME.xpi" *
 cp $NAME.xpi ..
 cd ..
 rm -rf $TEMP_DIR
