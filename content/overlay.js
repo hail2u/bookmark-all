bab61ef6a6e306a3e76fb4cd7e9135fa835531ff
Kyo Nagashima <kyo@hail2u.net>
2009-04-29 12:18:17 +0900

    add path preference for folder creates in

--- content/overlay.js
+++ content/overlay.js
@@ -1,11 +1,29 @@
 var bookmarkAll = {
   bookmarkAllTabs: function() {
-    var strFolderName = nsPreferences.copyUnicharPref(bookmarkAllPrefs.key, bookmarkAllPrefs.default) + this.getDateTime();
+    // 設定のロード
+    var prefs = bookmarkAllPrefs;
+    var strPrefPath   = nsPreferences.copyUnicharPref(prefs.PATH_KEY, prefs.PATH_DEFAULT);
+    var strPrefPrefix = nsPreferences.copyUnicharPref(prefs.PREFIX_KEY, prefs.PREFIX_DEFAULT);
 
+    // ブックマーク・サービス
     var bookmarksService = Components.classes["@mozilla.org/browser/nav-bookmarks-service;1"].getService(Components.interfaces.nsINavBookmarksService);
-    var menuFolder = bookmarksService.bookmarksMenuFolder;
-    var newFolderId = bookmarksService.createFolder(menuFolder, strFolderName, bookmarksService.DEFAULT_INDEX);
 
+    // ブックマークのルートを取得
+    var targetFolder = bookmarksService.bookmarksMenuFolder;
+
+    // ブックマークを格納するフォルダを格納するパスがなかったら作成
+    if (strPrefPath) {
+      strPrefPath.split("/").forEach(function (s) {
+        if (!bookmarksService.getChildFolder(targetFolder, s)) {
+          targetFolder = bookmarksService.createFolder(targetFolder, s, -1);
+        }
+      });
+    }
+
+    // ブックマークを格納するフォルダを作成
+    targetFolder = bookmarksService.createFolder(targetFolder, strPrefPrefix + this.getDateTime(), -1);
+
+    // 全てのタブをブックマーク
     var browsers = document.getElementById('content').browsers;
 
     for (var i = 0; i < browsers.length; ++i) {
@@ -21,7 +39,7 @@ var bookmarkAll = {
         name = url;
       }
 
-      var id = bookmarksService.insertBookmark(newFolderId, webNav.currentURI, -1, name);
+      var id = bookmarksService.insertBookmark(targetFolder, webNav.currentURI, -1, name);
     }
   },
 
--- content/prefs-overlay.js
+++ content/prefs-overlay.js
@@ -1,14 +1,20 @@
 var bookmarkAllPrefs = {
-  key:     "extensions.bookmark-all.prefix",
-  default: "Bookmarks at: ",
+  PATH_KEY:       "extensions.bookmark-all.path",
+  PATH_DEFAULT:   "",
+  PREFIX_KEY:     "extensions.bookmark-all.prefix",
+  PREFIX_DEFAULT: "Bookmarks at: ",
 
   loadPrefs: function() {
-    var strPrefPrefix = nsPreferences.copyUnicharPref(this.key, this.default);
+    var strPrefPath   = nsPreferences.copyUnicharPref(this.PATH_KEY, this.PATH_DEFAULT);
+    var strPrefPrefix = nsPreferences.copyUnicharPref(this.PREFIX_KEY, this.PREFIX_DEFAULT);
+    document.getElementById("prefPath").value   = strPrefPath;
     document.getElementById("prefPrefix").value = strPrefPrefix;
   },
 
   savePrefs: function() {
+    var strPrefPath   = document.getElementById("prefPath").value;
     var strPrefPrefix = document.getElementById("prefPrefix").value;
-    nsPreferences.setUnicharPref(this.key, strPrefPrefix);
+    nsPreferences.setUnicharPref(this.PATH_KEY, strPrefPath);
+    nsPreferences.setUnicharPref(this.PREFIX_KEY, strPrefPrefix);
   }
 };
